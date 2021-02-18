686ae856a30036309b599f0074d52246c8bb8e80
karthick <karthylock@gmail.com>
2021-02-18 17:25:32 +0530

    almost completed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const request = require("request");
 const https = require("https");
 
 const app = express();
@@ -17,7 +16,7 @@ app.post("/",function (req,res) {
     const lastName = req.body.lname;
     const email = req.body.email;
     const data = {
-        members = [
+        members: [
             {
                 email_address: email,
                 status:  "subscribed",
@@ -32,8 +31,18 @@ app.post("/",function (req,res) {
     const jsonData = JSON.stringify(data);
     console.log(firstName,lastName,email);
 
-   
-    https.request(url,options,function(response){
+    const url = "https://us1.api.mailchimp.com/3.0/lists/cdbcd94809";
+    const options = {
+        method: "POST",
+        auth: "karthy:c32a504b13277d128e04d57312616cf6-us1"
+    }
+   const request = https.request(url,options,function(response){
+
+    if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+    }else{
+        res.sendFile(__dirname + "/failure.html");
+    }
 
         response.on("data",function (data) {
             var dataa = JSON.parse(data);
@@ -41,9 +50,15 @@ app.post("/",function (req,res) {
         })
     })
 
+    request.write(jsonData);
+    request.end();
 })
 
-app.listen(3000,function(){
+app.post("/failure",function (req,res) {
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000,function(){
     console.log("server started on port 3000");
 })
 
