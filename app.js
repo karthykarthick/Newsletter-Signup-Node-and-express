67efe955080d681e85568cfbb76c254ec61bf785
karthick <karthylock@gmail.com>
2021-02-18 14:06:51 +0530

    finished ui part

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const bodyparser = require("body-parser");
 const https = require("https");
 
 const app = express();
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({urlencoded: true}));
 
 
 app.get("/",function(req,res) {
     res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/",function (req,res) {
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+    console.log(firstName,lastName,email);
 })
 
 app.listen(3000,function(){
