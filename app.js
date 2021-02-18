270af4ab3d94c8db6c01a54fe4835f411b157578
karthick <karthylock@gmail.com>
2021-02-18 13:53:30 +0530

    added Ui part for newsLetter part with node and express

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const bodyparser = require("body-parser");
 const https = require("https");
 
 const app = express();
+app.use(express.static("public"))
 
-app.get("/",function(req,res){
-    res.send("<h1>Hello ....</h1>");
+
+app.get("/",function(req,res) {
+    res.sendFile(__dirname + "/signup.html");
 })
-
-
-
-
-
-
 
 app.listen(3000,function(){
     console.log("server started on port 3000");
