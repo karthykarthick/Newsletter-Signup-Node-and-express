f8bf948348977c5a792101428868ef8168012282
karthick <karthylock@gmail.com>
2021-02-18 15:27:01 +0530

    added api key

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const request = require("request");
 const https = require("https");
 
 const app = express();
@@ -12,12 +13,39 @@ app.get("/",function(req,res) {
 })
 
 app.post("/",function (req,res) {
-    var firstName = req.body.fname;
-    var lastName = req.body.lname;
-    var email = req.body.email;
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    const data = {
+        members = [
+            {
+                email_address: email,
+                status:  "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
     console.log(firstName,lastName,email);
+
+    const url = "https://usX.api.mailchimp.com/3.0/lists"
+    https.request(url,options,function(response){
+
+    })
+
 })
 
 app.listen(3000,function(){
     console.log("server started on port 3000");
 })
+
+
+//Api key
+//c32a504b13277d128e04d57312616cf6-us1
+
+//List Id
+//cdbcd94809
